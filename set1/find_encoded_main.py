7b449215554d41ecfde672fd748182b8bbf606f1
shainer <syn.shainer@gmail.com>
2016-01-31 19:00:34 +0000

    Solved 4th problem.

---
+++ set1/find_encoded_main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import decode_xor
+
+if __name__ == '__main__':
+	strings = []
+
+	with open('data/encrypted.txt', 'r') as data_file:
+		strings = data_file.readlines()
+
+	decoder = decode_xor.XORDecoder(8)
+
+	for string in strings:
+		# Remove trailing newlines.
+		string = string.strip()
+
+		candidates = decoder.Decode(string)
+
+		# We need at least one candidate. For now we assume there is
+		# exactly one.
+		if candidates:
+			print ('String ' + string + ' decoded to ' + candidates[0][0])
+
