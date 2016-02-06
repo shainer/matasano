b5151abb7504eae7bde3116b6610689521de8fdb
shainer <syn.shainer@gmail.com>
2016-02-06 17:58:38 +0000

    Set 1 Challenge 8 solution.

---
+++ set1/ecb_detect.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+import collections
+
+def CountRepeatedBlocks(hex_string):
+	"""Counts how many 16-bytes chunks in the string are
+	repeated more than once. The input is an hexadecimal string."""
+	chunk_start = 0
+	CHUNK_SIZE = 32
+
+	# New keys get a default value of 0. The dictionary will
+	# count the number of occurrences of each 16-bytes chunk in
+	# the string.
+	chunkCounter = collections.defaultdict(int)
+
+	while chunk_start < len(hex_string):
+		chunk = hex_string[chunk_start : chunk_start + CHUNK_SIZE]
+		chunkCounter[chunk] += 1
+		chunk_start += CHUNK_SIZE
+
+	# This generator adds 1 for each value in the dictionary that
+	# is greater than 1.
+	return sum(1 for c in chunkCounter if chunkCounter[c] > 1)
+
+
+if __name__ == '__main__':
+	ecb_candidates = []
+
+	with open('data/8.txt', 'r') as f:
+		ecb_candidates = f.readlines()
+
+	repetitions = {}
+
+	for c in ecb_candidates:
+		candidate = c.strip()  # eliminates trailing newline
+		repeatedCount = CountRepeatedBlocks(candidate)
+
+		if repeatedCount > 0:
+			repetitions[candidate] = repeatedCount
+
+	for c in repetitions:
+		print (c + ' has ' + str(repetitions[c]) +
+			   ' block(s) repeated more than once.')
