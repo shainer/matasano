8d307b736b16c8cab8311f69b716db1f0d8ceaaa
shainer <syn.shainer@gmail.com>
2016-02-27 19:26:06 +0000

    CTR decryption was ignoring the last bytes, fix that.

--- set3/ctr_decryption.py
+++ set3/ctr_decryption.py
@@ -5,8 +5,8 @@ from Crypto.Cipher import AES
 from Crypto.Util import Counter
 
 def BlockXOR(b1, b2):
-	"""XOR of two blocks of bytes."""
-	assert len(b1) == len(b2)
+	"""XOR of two blocks of bytes. b2 is allowed to be longer
+	than b1, the extra bytes will simply be ignored."""
 	res = b''
 
 	for bIndex in range(0, len(b1)):
@@ -21,11 +21,21 @@ def DoCTR(text, key, nonce):
 	resultingText = b''
 
 	keystream = b''
+	# There is no padding in CTR mode, so both a plaintext and a ciphertext
+	# can be of non-exact block size. We decrypt the last block by generating
+	# the keystream and only using the bytes we need.
 	numBlocks = int(len(text) / AES.block_size)
 	counter = 0
 
-	for blockIndex in range(0, numBlocks):
-		block = text[blockIndex * AES.block_size : (blockIndex + 1) * AES.block_size]
+	# +1 to get the final bytes that do not fit in a block.
+	for blockIndex in range(0, numBlocks + 1):
+		# If at the end, simply get all the remaining bytes; there
+		# will be exactly AES.block_size or less of them. Otherwise,
+		# get a block.
+		if blockIndex == numBlocks:
+			block = text[blockIndex * AES.block_size :]
+		else:
+			block = text[blockIndex * AES.block_size : (blockIndex + 1) * AES.block_size]
 
 		# The keystream generator is composed by 8 bytes of the nonce +
 		# 8 bytes of counter, little-endian.
@@ -41,7 +51,7 @@ def DoCTR(text, key, nonce):
 		# The PT is simply the CT XORed with the keystream, and vice versa.
 		# Since the PT only depends on the same block of CT plus a predictable
 		# keystream, CTR is very suited for parallel decryption of many blocks.
-		resultingText += BlockXOR(keystream, block)
+		resultingText += BlockXOR(block, keystream)
 		counter += 1
 
 	return resultingText
