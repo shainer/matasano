6e465224a057eb8af3404d6f402387fb8c625dd1
shainer <syn.shainer@gmail.com>
2016-02-27 19:21:19 +0000

    Modify the naming since CTR is the same when encrypting or decrypting.

--- set3/ctr_decryption.py
+++ set3/ctr_decryption.py
@@ -14,40 +14,40 @@ def BlockXOR(b1, b2):
 
 	return res
 
-def CTRDecrypt(ciphertext, key, nonce):
+def DoCTR(text, key, nonce):
+	"""The algorithm is the same for both encryption and decryption."""
 	# Uses ECB inside for a single block.
 	cipher = AES.new(key=key, mode=AES.MODE_ECB)
-	plaintext = b''
+	resultingText = b''
 
 	keystream = b''
-	numBlocks = int(len(ciphertext) / AES.block_size)
+	numBlocks = int(len(text) / AES.block_size)
 	counter = 0
 
 	for blockIndex in range(0, numBlocks):
-		block = ciphertext[blockIndex * AES.block_size : (blockIndex + 1) * AES.block_size]
+		block = text[blockIndex * AES.block_size : (blockIndex + 1) * AES.block_size]
 
 		# The keystream generator is composed by 8 bytes of the nonce +
 		# 8 bytes of counter, little-endian.
 		keystreamGen = bytes([nonce]) * 8
 		# This is a quick hack that only works if there are not enough
-		# block of ciphertext to need the second digit of the counter too.
+		# blocks of text to need the second digit of the counter too.
 		# Since that is a lot of blocks we can be a bit lazy.
 		keystreamGen += bytes([counter, 0, 0, 0, 0, 0, 0, 0])
 
-		# Careful, we always encrypt the keystream even when decrypting
-		# some text.
+		# Careful, we always encrypt the keystream even when decrypting.
 		keystream = cipher.encrypt(keystreamGen)
 
-		# The PT is simply the CT XORed with the keystream.
+		# The PT is simply the CT XORed with the keystream, and vice versa.
 		# Since the PT only depends on the same block of CT plus a predictable
 		# keystream, CTR is very suited for parallel decryption of many blocks.
-		plaintext += BlockXOR(keystream, block)
+		resultingText += BlockXOR(keystream, block)
 		counter += 1
 
-	return plaintext
+	return resultingText
 
 if __name__ == '__main__':
 	c64 = 'L77na/nrFsKvynd6HzOoG7GHTLXsTVu9qvY/2syLXzhPweyyMTJULu/6/kXX0KSvoOLSFQ=='
 	ciphertext = base64.b64decode(c64)
 
-	print(CTRDecrypt(ciphertext, b'YELLOW SUBMARINE', 0).decode('ascii'))
+	print(DoCTR(ciphertext, b'YELLOW SUBMARINE', 0).decode('ascii'))
