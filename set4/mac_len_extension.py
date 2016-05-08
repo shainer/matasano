1fb6fcbef41dc65935597bd32bfb28761ac71605
shainer <syn.shainer@gmail.com>
2016-05-09 00:08:32 +0100

    Set 4, Challenge 29, partial solution (missing breaking the key length).

---
+++ set4/mac_len_extension.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+
+from sha1_mac import Sha1Sign
+from sha1_mac import Sha1Hash
+import hashlib
+import struct
+
+def makeGluePadding(message, keylen):
+	byteLength = len(message) + keylen
+	padding = b''
+
+	# Appends the bit '1' to the message.
+	padding += b'\x80'
+	# Appends the byte '0' k times; k is such that the resulting
+	# message length in bits is congruent to 64.
+	padding += b'\x00' * ((56 - (byteLength + 1) % 64) % 64)
+	# Appends the message length in bits as a 64-bit big-endian
+	# integer (Q is unsigned long long for the struct module).
+	padding += struct.pack(b'>Q', byteLength * 8)
+	return padding
+
+def RecoverInternalState(secretDecimalHash):
+	a = secretDecimalHash >> 128
+	b = (secretDecimalHash >> 96) & 0xffffffff
+	c = (secretDecimalHash >> 64) & 0xffffffff
+	d = (secretDecimalHash >> 32) & 0xffffffff
+	e = secretDecimalHash & 0xffffffff
+	return [a, b, c, d, e]
+
+def GetRealDigest(message):
+	sha = hashlib.sha1()
+	sha.update(b'YELLOW SUBMARINE')
+	sha.update(message)
+	return sha.hexdigest()
+
+# FIXME: here we assume to know the exact length of the secret key
+# used to produce the MAC. We need our algorithm to discover the
+# length itself.
+if __name__ == '__main__':
+	message = b'comment1=cooking%20MCs;userdata=foo;comment2=%20like%20a%20pound%20of%20bacon'
+	secretHash = Sha1Sign(message)
+	decimalHash = int(secretHash, 16)
+
+	forgedPart = b';admin=true'
+
+	# The new message length is the length of the key + the
+	# original message + the artificial padding glue + the
+	# part we added ourselves.
+	glue = makeGluePadding(message, 16)
+	# After the forget part, the SHA1 algorithm may add more
+	# padding, but that happens internally so we do not need
+	# to factor its length here.
+	fakeLen = 16 + len(message) + len(glue) + len(forgedPart)
+
+	# Create a SHA1 object with our how H vector. This allows us
+	# to extend the hash from where we started.
+	h = RecoverInternalState(decimalHash)
+	sha1 = Sha1Hash(h)
+	digest = sha1.digest(b';admin=true', fakeLen)
+	realThing = GetRealDigest(message + glue + forgedPart)
+
+	# We expect them to be equal for the attack to succeed.
+	print(digest)
+	print(realThing)
