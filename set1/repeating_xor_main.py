304f543dec1f63b138932d4e3ccfaeb10271fb99
shainer <syn.shainer@gmail.com>
2017-05-28 15:08:59 +0100

    Remove unused file.

--- set1/repeating_xor_main.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-import encode_xor
-
-if __name__ == '__main__':
-	encoder = encode_xor.XOREncoder()
-	text = 'Burning \'em, if you ain\'t quick and nimble\nI go crazy when I hear a cymbal'
-
-	print(encoder.Encode(text, 'ICE'))
